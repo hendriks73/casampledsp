983811c7a62a2f50866227c533a5129e98a500ac
Hendrik Schreiber <hs@tagtraum.com>
2016-05-05 17:24:13 +0200

    Improved exception messages.

--- casampledsp-java/src/main/java/com/tagtraum/casampledsp/CANativeLibraryLoader.java
+++ casampledsp-java/src/main/java/com/tagtraum/casampledsp/CANativeLibraryLoader.java
@@ -218,7 +218,7 @@ public final class CANativeLibraryLoader {
                     while (((i+2) < numChars) && (c=='%')) {
                         int v = Integer.parseInt(s.substring(i+1,i+3),16);
                         if (v < 0) {
-                            throw new IllegalArgumentException("NativeLibraryLoader: Illegal hex characters in escape (%) pattern - negative value");
+                            throw new IllegalArgumentException("CANativeLibraryLoader: Illegal hex characters in escape (%) pattern - negative value");
                         }
                         bytes[pos++] = (byte) v;
                         i+= 3;
@@ -230,12 +230,12 @@ public final class CANativeLibraryLoader {
                     // A trailing, incomplete byte encoding such as
                     // "%x" will cause an exception to be thrown
                     if ((i < numChars) && (c=='%')) {
-                        throw new IllegalArgumentException("NativeLibraryLoader: Incomplete trailing escape (%) pattern");
+                        throw new IllegalArgumentException("CANativeLibraryLoader: Incomplete trailing escape (%) pattern");
                     }
 
                     sb.append(new String(bytes, 0, pos, "UTF-8"));
                 } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("NativeLibraryLoader: Illegal hex characters in escape (%) pattern - " + e.getMessage());
+                    throw new IllegalArgumentException("CANativeLibraryLoader: Illegal hex characters in escape (%) pattern - " + e.getMessage());
                 }
                 needToChange = true;
             } else {
