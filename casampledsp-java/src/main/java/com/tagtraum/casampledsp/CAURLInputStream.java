5a53a464612420a17bb14e5c6ad95b0721a3d9d6
Hendrik Schreiber <hs@tagtraum.com>
2022-01-02 14:46:28 +0100

    Fixed copyright notice.

--- casampledsp-java/src/main/java/com/tagtraum/casampledsp/CAURLInputStream.java
+++ casampledsp-java/src/main/java/com/tagtraum/casampledsp/CAURLInputStream.java
@@ -3,18 +3,18 @@
  * Copyright 2011 tagtraum industries incorporated
  * This file is part of CASampledSP.
  *
- * FFSampledSP is free software; you can redistribute it and/or
+ * CASampledSP is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFSampledSP is distributed in the hope that it will be useful,
+ * CASampledSP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFSampledSP; if not, write to the Free Software
+ * License along with CASampledSP; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  * =================================================
  */
